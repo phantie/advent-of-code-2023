6c1fbd4d1f9fbc4c116ccf95cebd457d1a9372ec
phantie <funtom1605@gmail.com>
2023-12-02 09:38:12 +0200

    parse day 2 with nom

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 mod part_one {
     use super::*;
 
@@ -103,4 +105,98 @@ fn main() {
 fn read_input() -> utils::ReadLines {
     let filename = "input.txt";
     utils::read_lines(filename).unwrap()
+}
+
+mod parse {
+    use nom::{branch::alt, bytes::complete::tag, character, multi::separated_list1, IResult};
+
+    #[derive(Debug, PartialEq)]
+    pub struct CountColor<'a> {
+        pub count: u32,
+        pub color: &'a str,
+    }
+
+    type Game<'a> = Vec<CountColor<'a>>;
+    type GameSeq<'a> = Vec<Game<'a>>;
+
+    fn parse_count_color(input: &str) -> IResult<&str, CountColor> {
+        let (input, count) = character::complete::u32(input)?;
+        let (input, _) = tag(" ")(input)?;
+        let (input, color) = alt((tag("red"), tag("green"), tag("blue")))(input)?;
+        Ok((input, CountColor { count, color }))
+    }
+
+    fn parse_game(input: &str) -> IResult<&str, Game> {
+        let (input, result) = separated_list1(tag(", "), parse_count_color)(input)?;
+        Ok((input, result))
+    }
+
+    fn parse_games(input: &str) -> IResult<&str, GameSeq> {
+        let (input, result) = separated_list1(tag("; "), parse_game)(input)?;
+        Ok((input, result))
+    }
+
+    pub fn parse_line(input: &str) -> IResult<&str, (u32, GameSeq)> {
+        let (input, _) = tag("Game ")(input)?;
+        let (input, id) = character::complete::u32(input)?;
+        let (input, _) = tag(": ")(input)?;
+        let (input, game) = parse_games(input)?;
+        Ok((input, (id, game)))
+    }
+
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test_parse_line() {
+            use super::CountColor;
+
+            let (remaining, (id, games)) = super::parse_line(
+                "Game 22: 3 red, 1 blue; 3 green, 1 red, 1 blue; 7 green, 2 blue",
+            )
+            .unwrap();
+
+            assert!(remaining.is_empty());
+
+            assert_eq!(id, 22);
+            assert_eq!(
+                games,
+                vec![
+                    vec![
+                        CountColor {
+                            count: 3,
+                            color: "red",
+                        },
+                        CountColor {
+                            count: 1,
+                            color: "blue",
+                        },
+                    ],
+                    vec![
+                        CountColor {
+                            count: 3,
+                            color: "green",
+                        },
+                        CountColor {
+                            count: 1,
+                            color: "red",
+                        },
+                        CountColor {
+                            count: 1,
+                            color: "blue",
+                        },
+                    ],
+                    vec![
+                        CountColor {
+                            count: 7,
+                            color: "green",
+                        },
+                        CountColor {
+                            count: 2,
+                            color: "blue",
+                        },
+                    ],
+                ]
+            );
+        }
+    }
 }
