bd5ec8ddc83e9cb7febfcad5694328a1b184321a
phantie <funtom1605@gmail.com>
2023-12-03 10:54:35 +0200

    day 3 extract common functionality

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,6 @@
-#![allow(unused)]
-
 mod part_one {
+    use super::imports::*;
+
     pub fn part_one() -> u32 {
         find(
             super::read_input()
@@ -10,63 +10,22 @@ mod part_one {
         )
     }
 
-    #[derive(strum::EnumIs, Clone)]
-    enum Cell {
-        Number(u32),
-        Symbol,
-        Empty,
-    }
-
-    fn parse_line(value: String) -> Vec<Cell> {
-        std::iter::once(Cell::Empty)
-            .chain(value.chars().map(|c| match c {
-                c if c.is_numeric() => Cell::Number(c.to_string().parse().unwrap()),
-                c if c == '.' => Cell::Empty,
-                _c => Cell::Symbol,
-            }))
-            .chain(std::iter::once(Cell::Empty))
-            .collect()
-    }
-
     fn find(rows: Vec<Vec<Cell>>) -> u32 {
-        let row_len = rows[0].len();
-        let empty_row = (0..row_len).map(|_| Cell::Empty).collect::<Vec<_>>();
-
-        let rows = std::iter::once(empty_row.clone())
-            .chain(rows)
-            .chain(std::iter::once(empty_row.clone()))
-            .collect::<Vec<_>>();
+        let rows = pad_space(rows);
 
         let mut total = 0;
 
         for (i, row) in rows.iter().enumerate() {
+            // padded empty rows
             if i == 0 || i == rows.len() - 1 {
                 continue;
             }
 
-            let prev_row = rows[i - 1].clone();
-            let next_row = rows[i + 1].clone();
+            let prev_row = &rows[i - 1];
+            let next_row = &rows[i + 1];
 
-            let mut numbers = vec![];
-            let mut finished_num = true;
-            for (i, cell) in row.iter().enumerate() {
-                if !cell.is_number() {
-                    finished_num = true
-                }
-
-                if let Cell::Number(num) = cell {
-                    if finished_num {
-                        numbers.push((i, *num));
-                        finished_num = false;
-                    } else {
-                        let (_i, last) = numbers.last_mut().unwrap();
-                        *last = *last * 10 + num;
-                    }
-                }
-            }
-
-            for (i, num) in numbers.clone() {
-                let num_len = (num.checked_ilog10().unwrap_or(0) + 1) as usize;
+            for (i, num) in extract_nums(row) {
+                let num_len = num_len(num);
 
                 let sym_precedes = row[i - 1].is_symbol();
                 let sym_succeeds = row[i + num_len].is_symbol();
@@ -82,102 +41,42 @@ mod part_one {
 
         total
     }
-}
 
-fn read_input() -> utils::ReadLines {
-    let filename = "input.txt";
-    utils::read_lines(filename).unwrap()
-}
-
-fn main() {
-    // let result = part_one::part_one();
-    // println!("result: {result}");
-    let result = part_two::part_two();
-    println!("result: {result}");
+    #[cfg(test)]
+    #[test]
+    fn test_part_one() {
+        assert_eq!(part_one(), 535351);
+    }
 }
 
 mod part_two {
+    use super::imports::*;
+
     pub fn part_two() -> u32 {
-        let r = find(
+        find(
             super::read_input()
                 .map(Result::unwrap)
                 .map(parse_line)
                 .collect::<Vec<_>>(),
-        );
-
-        r
-    }
-
-    fn num_len(num: u32) -> usize {
-        (num.checked_ilog10().unwrap_or(0) + 1) as usize
-    }
-
-    #[derive(strum::EnumIs, Clone)]
-    enum Cell {
-        Number(u32),
-        Symbol,
-        Empty,
-    }
-
-    fn parse_line(value: String) -> Vec<Cell> {
-        std::iter::once(Cell::Empty)
-            .chain(value.chars().map(|c| match c {
-                c if c.is_numeric() => Cell::Number(c.to_string().parse().unwrap()),
-                c if c == '.' => Cell::Empty,
-                _c => Cell::Symbol,
-            }))
-            .chain(std::iter::once(Cell::Empty))
-            .collect()
-    }
-
-    fn extract_nums(row: &Vec<Cell>) -> Vec<(usize, u32)> {
-        let mut numbers = vec![];
-        let mut finished_num = true;
-        for (i, cell) in row.iter().enumerate() {
-            if !cell.is_number() {
-                finished_num = true
-            }
-
-            if let Cell::Number(num) = cell {
-                if finished_num {
-                    numbers.push((i, *num));
-                    finished_num = false;
-                } else {
-                    let (_i, last) = numbers.last_mut().unwrap();
-                    *last = *last * 10 + num;
-                }
-            }
-        }
-        numbers
+        )
     }
 
     fn find(rows: Vec<Vec<Cell>>) -> u32 {
-        let row_len = rows[0].len();
-        let empty_row = (0..row_len).map(|_| Cell::Empty).collect::<Vec<_>>();
-
-        let rows = std::iter::once(empty_row.clone())
-            .chain(rows)
-            .chain(std::iter::once(empty_row.clone()))
-            .collect::<Vec<_>>();
+        let rows = pad_space(rows);
 
         let mut total = 0;
 
         for (i, row) in rows.iter().enumerate() {
+            // padded empty rows
             if i == 0 || i == rows.len() - 1 {
                 continue;
             }
 
-            let prev_row = rows[i - 1].clone();
-            let next_row = rows[i + 1].clone();
+            let prev_row = &rows[i - 1];
+            let next_row = &rows[i + 1];
 
-            let mut symbols = vec![];
-            for (i, cell) in row.iter().enumerate() {
-                if let Cell::Symbol = cell {
-                    symbols.push(i);
-                }
-            }
-
-            let numbers = extract_nums(&row);
+            let symbols = extract_syms(row);
+            let numbers = extract_nums(row);
 
             for i in symbols {
                 let matched_nums = numbers
@@ -188,12 +87,12 @@ mod part_two {
                     })
                     .collect::<Vec<_>>();
 
-                let prev_row_matched = extract_nums(&prev_row)
+                let prev_row_matched = extract_nums(prev_row)
                     .into_iter()
                     .filter(|(n_i, num)| ((*n_i - 1)..=(n_i + num_len(*num))).any(|n_i| n_i == i))
                     .collect::<Vec<_>>();
 
-                let next_row_matched = extract_nums(&next_row)
+                let next_row_matched = extract_nums(next_row)
                     .into_iter()
                     .filter(|(n_i, num)| ((*n_i - 1)..=(n_i + num_len(*num))).any(|n_i| n_i == i))
                     .collect::<Vec<_>>();
@@ -216,4 +115,83 @@ mod part_two {
     fn test_part_two() {
         assert_eq!(part_two(), 87287096);
     }
+}
+
+mod imports {
+    #[derive(strum::EnumIs, Clone)]
+    pub enum Cell {
+        Number(u32),
+        Symbol,
+        Empty,
+    }
+
+    pub fn parse_line(value: String) -> Vec<Cell> {
+        std::iter::once(Cell::Empty)
+            .chain(value.chars().map(|c| match c {
+                c if c.is_numeric() => Cell::Number(c.to_string().parse().unwrap()),
+                c if c == '.' => Cell::Empty,
+                _c => Cell::Symbol,
+            }))
+            .chain(std::iter::once(Cell::Empty))
+            .collect()
+    }
+
+    pub fn pad_space(rows: Vec<Vec<Cell>>) -> Vec<Vec<Cell>> {
+        // pad empty with empty rows and columns to ease indexing
+        //
+        let row_len = rows[0].len();
+        let empty_row = (0..row_len).map(|_| Cell::Empty).collect::<Vec<_>>();
+
+        std::iter::once(empty_row.clone())
+            .chain(rows)
+            .chain(std::iter::once(empty_row.clone()))
+            .collect()
+    }
+
+    pub fn num_len(num: u32) -> usize {
+        (num.checked_ilog10().unwrap_or(0) + 1) as usize
+    }
+
+    pub fn extract_syms(row: &Vec<Cell>) -> Vec<usize> {
+        row.into_iter()
+            .enumerate()
+            .filter(|(_i, cell)| cell.is_symbol())
+            .map(|(i, _cell)| i)
+            .collect()
+    }
+
+    pub fn extract_nums(row: &Vec<Cell>) -> Vec<(usize, u32)> {
+        let mut numbers = vec![];
+
+        let mut finished_num = true;
+        for (i, cell) in row.iter().enumerate() {
+            if !cell.is_number() {
+                finished_num = true
+            }
+
+            if let Cell::Number(num) = cell {
+                if finished_num {
+                    numbers.push((i, *num));
+                    finished_num = false;
+                } else {
+                    let (_i, last) = numbers.last_mut().unwrap();
+                    *last = *last * 10 + num;
+                }
+            }
+        }
+
+        numbers
+    }
+}
+
+fn read_input() -> utils::ReadLines {
+    let filename = "input.txt";
+    utils::read_lines(filename).unwrap()
+}
+
+fn main() {
+    let result = part_one::part_one();
+    println!("result: {result}");
+    let result = part_two::part_two();
+    println!("result: {result}");
 }
