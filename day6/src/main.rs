80950c7492b898122cf527d107ec2abfa420d65b
phantie <funtom1605@gmail.com>
2023-12-06 10:16:09 +0200

    day 6 part 2

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,12 @@
 use part_one::part_one;
 
+use crate::part_two::part_two;
+
 fn main() {
     let (_, races) = parse::parse_input(input()).unwrap();
 
-    dbg!(part_one(races)); // 219849
+    dbg!(part_one(races.clone())); // 219849
+    dbg!(part_two(races)); // 29432455
 }
 
 mod part_one {
@@ -12,6 +15,27 @@ mod part_one {
             .into_iter()
             .map(|race| race.ways_to_beat_the_record())
             .fold(1, |acc, x| acc * x)
+    }
+}
+
+mod part_two {
+    pub fn part_two(races: Vec<crate::parse::Race>) -> usize {
+        let (times, distances) =
+            races
+                .into_iter()
+                .fold((String::new(), String::new()), |(times, distances), x| {
+                    (
+                        format!("{times}{}", x.time),
+                        format!("{distances}{}", x.distance),
+                    )
+                });
+
+        let race = crate::parse::Race {
+            time: times.parse().unwrap(),
+            distance: distances.parse().unwrap(),
+        };
+
+        race.ways_to_beat_the_record()
     }
 }
 
@@ -25,8 +49,8 @@ mod parse {
 
     #[derive(Debug, Clone, Copy)]
     pub struct Race {
-        time: u32,
-        distance: u32,
+        pub time: u64,
+        pub distance: u64,
     }
 
     impl Race {
@@ -44,12 +68,12 @@ mod parse {
     pub fn parse_input(input: &str) -> IResult<&str, Vec<Race>> {
         let (input, _) = tag("Time:")(input)?;
         let (input, _) = space1(input)?;
-        let (input, times) = separated_list1(complete::space1, complete::u32)(input)?;
+        let (input, times) = separated_list1(complete::space1, complete::u64)(input)?;
         let (input, _) = complete::newline(input)?;
 
         let (input, _) = tag("Distance:")(input)?;
         let (input, _) = space1(input)?;
-        let (input, distances) = separated_list1(complete::space1, complete::u32)(input)?;
+        let (input, distances) = separated_list1(complete::space1, complete::u64)(input)?;
 
         Ok((
             input,
