29bc655c13395bbced6ab0970c9547820354b269
phantie <funtom1605@gmail.com>
2023-12-09 15:25:14 +0200

    day 9 part 2

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,16 +1,23 @@
 use std::str::FromStr;
 
 fn main() {
-    dbg!(read_input().next());
-
-    let part_one = part_one::part_one();
+    let part_one = part_one::part_one(); // 1637452029
     dbg!(part_one);
 
-    // let part_one = test_input()
-    //     .into_iter()
-    //     .map(parse_line)
-    //     .map(predict_next)
-    //     .sum::<SeqItem>();
+    let part_two = part_two::part_two(); // 908
+    dbg!(part_two);
+}
+
+mod part_two {
+    use super::*;
+
+    pub fn part_two() -> SeqItem {
+        read_input()
+            .map(Result::unwrap)
+            .map(parse_line)
+            .map(predict_first)
+            .sum::<SeqItem>()
+    }
 }
 
 mod part_one {
@@ -20,7 +27,7 @@ mod part_one {
         read_input()
             .map(Result::unwrap)
             .map(parse_line)
-            .map(predict_next)
+            .map(predict_first)
             .sum::<SeqItem>()
     }
 }
@@ -36,8 +43,8 @@ fn parse_line(value: String) -> Seq {
 #[allow(unused)]
 fn test_input() -> Vec<String> {
     "0 3 6 9 12 15
-    1 3 6 10 15 21
-    10 13 16 21 30 45"
+1 3 6 10 15 21
+10 13 16 21 30 45"
         .split("\n")
         .map(ToOwned::to_owned)
         .collect()
@@ -79,5 +86,36 @@ fn predict_next(seq: Seq) -> SeqItem {
     diffs.as_slice().windows(2).rev().fold(0, |acc, window| {
         let (upper, _lower) = (&window[0], &window[1]);
         upper.last().unwrap() + acc
+    })
+}
+
+fn predict_first(seq: Seq) -> SeqItem {
+    let mut diffs = vec![seq];
+
+    loop {
+        let diff_seq = diffs
+            .last()
+            .unwrap()
+            .as_slice()
+            .windows(2)
+            .map(|window| {
+                let (l, r) = (window[0], window[1]);
+                let diff = r - l;
+                diff
+            })
+            .collect::<Vec<_>>();
+
+        let all_zeroes = diff_seq.iter().all(|v| *v == 0);
+
+        diffs.push(diff_seq);
+
+        if all_zeroes {
+            break;
+        }
+    }
+
+    diffs.as_slice().windows(2).rev().fold(0, |acc, window| {
+        let (upper, _lower) = (&window[0], &window[1]);
+        upper.first().unwrap() - acc
     })
 }
