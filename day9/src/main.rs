f2a4411d6b5cd3dd6890b8db7dd1fbe3c498e08b
phantie <funtom1605@gmail.com>
2023-12-09 15:32:52 +0200

    day 9 refactor predict fns into one

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,27 +1,44 @@
-fn main() {
-    let part_one = part_one::part_one(); // 1637452029
-    dbg!(part_one);
+type SeqItem = i32;
+type Seq = Vec<SeqItem>;
 
-    let part_two = part_two::part_two(); // 908
-    dbg!(part_two);
+enum Predict {
+    Prev,
+    Next,
 }
 
-mod part_two {
-    use super::*;
+fn predict(mode: Predict, seq: Seq) -> SeqItem {
+    let mut diffs = vec![seq];
 
-    pub fn part_two() -> SeqItem {
-        read_input()
-            .map(Result::unwrap)
-            .map(parse_line)
-            .map(predict_first)
-            .sum::<SeqItem>()
+    loop {
+        let diff_seq = diffs
+            .last()
+            .unwrap()
+            .as_slice()
+            .windows(2)
+            .map(|window| {
+                let (l, r) = (window[0], window[1]);
+                let diff = r - l;
+                diff
+            })
+            .collect::<Vec<_>>();
+
+        let all_zeroes = diff_seq.iter().all(|v| *v == 0);
+
+        diffs.push(diff_seq);
+
+        if all_zeroes {
+            break;
+        }
     }
 
-    #[cfg(test)]
-    #[test]
-    fn test_part_two() {
-        assert_eq!(part_two(), 908);
-    }
+    diffs.as_slice().windows(2).rev().fold(0, |acc, window| {
+        let (upper, _lower) = (&window[0], &window[1]);
+        if matches!(mode, Predict::Next) {
+            upper.last().unwrap() + acc
+        } else {
+            upper.first().unwrap() - acc
+        }
+    })
 }
 
 mod part_one {
@@ -31,7 +48,7 @@ mod part_one {
         read_input()
             .map(Result::unwrap)
             .map(parse_line)
-            .map(predict_next)
+            .map(|seq| predict(Predict::Next, seq))
             .sum::<SeqItem>()
     }
 
@@ -42,6 +59,29 @@ mod part_one {
     }
 }
 
+mod part_two {
+    use super::*;
+
+    pub fn part_two() -> SeqItem {
+        read_input()
+            .map(Result::unwrap)
+            .map(parse_line)
+            .map(|seq| predict(Predict::Prev, seq))
+            .sum::<SeqItem>()
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(), 908);
+    }
+}
+
+fn read_input() -> utils::ReadLines {
+    let filename = "input.txt";
+    utils::read_lines(filename).unwrap()
+}
+
 fn parse_line(value: String) -> Seq {
     value
         .split(" ")
@@ -50,72 +90,10 @@ fn parse_line(value: String) -> Seq {
         .collect()
 }
 
-fn read_input() -> utils::ReadLines {
-    let filename = "input.txt";
-    utils::read_lines(filename).unwrap()
-}
+fn main() {
+    let part_one = part_one::part_one(); // 1637452029
+    dbg!(part_one);
 
-type SeqItem = i32;
-type Seq = Vec<SeqItem>;
-
-fn predict_next(seq: Seq) -> SeqItem {
-    let mut diffs = vec![seq];
-
-    loop {
-        let diff_seq = diffs
-            .last()
-            .unwrap()
-            .as_slice()
-            .windows(2)
-            .map(|window| {
-                let (l, r) = (window[0], window[1]);
-                let diff = r - l;
-                diff
-            })
-            .collect::<Vec<_>>();
-
-        let all_zeroes = diff_seq.iter().all(|v| *v == 0);
-
-        diffs.push(diff_seq);
-
-        if all_zeroes {
-            break;
-        }
-    }
-
-    diffs.as_slice().windows(2).rev().fold(0, |acc, window| {
-        let (upper, _lower) = (&window[0], &window[1]);
-        upper.last().unwrap() + acc
-    })
-}
-
-fn predict_first(seq: Seq) -> SeqItem {
-    let mut diffs = vec![seq];
-
-    loop {
-        let diff_seq = diffs
-            .last()
-            .unwrap()
-            .as_slice()
-            .windows(2)
-            .map(|window| {
-                let (l, r) = (window[0], window[1]);
-                let diff = r - l;
-                diff
-            })
-            .collect::<Vec<_>>();
-
-        let all_zeroes = diff_seq.iter().all(|v| *v == 0);
-
-        diffs.push(diff_seq);
-
-        if all_zeroes {
-            break;
-        }
-    }
-
-    diffs.as_slice().windows(2).rev().fold(0, |acc, window| {
-        let (upper, _lower) = (&window[0], &window[1]);
-        upper.first().unwrap() - acc
-    })
+    let part_two = part_two::part_two(); // 908
+    dbg!(part_two);
 }
