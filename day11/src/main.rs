feadc59b3b15910c6ba8adfc1a930dc280d067f9
phantie <funtom1605@gmail.com>
2023-12-11 15:43:30 +0200

    day 11 part two

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,23 +1,9 @@
-fn main() {
-    let r = part_one::part_one();
-    dbg!(r);
-}
-
 mod part_one {
     use super::*;
 
     pub fn part_one() -> usize {
-        let space = Space(read_input().map(Result::unwrap).map(parse_line).collect());
-
-        let empty_rows = space.empty_rows();
-        let empty_columns = space.empty_columns();
-
-        let galaxy_pairs = space.galaxy_pairs();
-
-        galaxy_pairs
-            .into_iter()
-            .map(|(l, r)| shortest_distance(l, r, &empty_rows, &empty_columns))
-            .sum::<usize>()
+        let space = Space::default();
+        space.shortest_distances(1)
     }
 
     #[cfg(test)]
@@ -27,42 +13,39 @@ mod part_one {
     }
 }
 
-fn read_input() -> utils::ReadLines {
-    let filename = "input.txt";
-    utils::read_lines(filename).unwrap()
-}
+mod part_two {
+    use super::*;
 
-type I = usize;
-type J = usize;
-type Pos = (I, J);
+    pub fn part_two() -> usize {
+        let space = Space::default();
+        space.shortest_distances(10usize.pow(6) - 1)
+    }
 
-type SpaceRow = Vec<Cell>;
-
-struct Space(Vec<SpaceRow>);
-
-impl std::ops::Deref for Space {
-    type Target = Vec<SpaceRow>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+    #[cfg(test)]
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(), 630728425490);
     }
 }
 
 pub fn shortest_distance(
     (y_l, x_l): Pos,
     (y_r, x_r): Pos,
+    expansion_multiplier: usize,
     empty_rows: &Vec<usize>,
     empty_columns: &Vec<usize>,
 ) -> usize {
     let x_expansion = empty_columns
         .into_iter()
         .filter(|i| **i > x_l.min(x_r) && **i < x_r.max(x_l))
-        .count();
+        .count()
+        * expansion_multiplier;
 
     let y_expansion = empty_rows
         .into_iter()
         .filter(|i| **i > y_l.min(y_r) && **i < y_r.max(y_l))
-        .count();
+        .count()
+        * expansion_multiplier;
 
     ((x_r as isize - x_l as isize).abs() + (y_r as isize - y_l as isize).abs()) as usize
         + x_expansion
@@ -70,6 +53,22 @@ pub fn shortest_distance(
 }
 
 impl Space {
+    pub fn shortest_distances(&self, expansion_multiplier: usize) -> usize {
+        let space = self;
+
+        let empty_rows = space.empty_rows();
+        let empty_columns = space.empty_columns();
+
+        let galaxy_pairs = space.galaxy_pairs();
+
+        galaxy_pairs
+            .into_iter()
+            .map(|(l, r)| {
+                shortest_distance(l, r, expansion_multiplier, &empty_rows, &empty_columns)
+            })
+            .sum::<usize>()
+    }
+
     pub fn galaxy_pairs(&self) -> Vec<(Pos, Pos)> {
         let galaxies = self.galaxies();
 
@@ -115,6 +114,33 @@ enum Cell {
     Universe,
 }
 
+type I = usize;
+type J = usize;
+type Pos = (I, J);
+
+type SpaceRow = Vec<Cell>;
+
+struct Space(Vec<SpaceRow>);
+
+impl Default for Space {
+    fn default() -> Self {
+        Self(read_input().map(Result::unwrap).map(parse_line).collect())
+    }
+}
+
+impl std::ops::Deref for Space {
+    type Target = Vec<SpaceRow>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn read_input() -> utils::ReadLines {
+    let filename = "input.txt";
+    utils::read_lines(filename).unwrap()
+}
+
 fn parse_line(value: String) -> SpaceRow {
     value
         .chars()
@@ -124,4 +150,11 @@ fn parse_line(value: String) -> SpaceRow {
             _ => unreachable!(),
         })
         .collect()
+}
+
+fn main() {
+    let part_one = part_one::part_one();
+    dbg!(part_one);
+    let part_two = part_two::part_two();
+    dbg!(part_two);
 }
