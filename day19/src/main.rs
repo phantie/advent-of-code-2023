40eef084a45c524bb8d2830d74a96aa73cf078a1
phantie <funtom1605@gmail.com>
2023-12-19 21:01:01 +0200

    day 19 part one

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -3,11 +3,31 @@
 fn main() {
     let (workflows, parts) = parse::parse_input(input());
 
-    dbg!(workflows, parts);
+    dbg!(&workflows, &parts);
+
+    let r = part_one(workflows, parts);
+    dbg!(r);
 }
 
-fn part_one(workflows: Workflows, parts: Parts) {
-    fn calc_part(part: Part, workflows: &Workflows) {}
+// 333263
+fn part_one(workflows: Workflows, parts: Parts) -> Num {
+    fn part_accepted(part: Part, workflows: &Workflows) -> bool {
+        let mut workflow = workflows.get("in").unwrap();
+
+        loop {
+            match workflow.process_part(part) {
+                Next::Accept => return true,
+                Next::Reject => return false,
+                Next::Send(name) => workflow = workflows.get(&name).unwrap(),
+            }
+        }
+    }
+
+    parts
+        .into_iter()
+        .filter(|part| part_accepted(*part, &workflows))
+        .map(|Part { x, m, a, s }| x + m + a + s)
+        .sum::<Num>()
 }
 
 fn input() -> &'static str {
@@ -16,7 +36,7 @@ fn input() -> &'static str {
 
 pub type Num = usize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum RatingLabel {
     X,
     M,
@@ -32,16 +52,22 @@ pub struct Workflow {
     ruleset: Ruleset,
 }
 
+impl Workflow {
+    fn process_part(&self, part: Part) -> Next {
+        self.ruleset.process_part(part)
+    }
+}
+
 pub type Workflows = std::collections::HashMap<WorkflowName, Workflow>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Next {
     Accept,
     Reject,
     Send(WorkflowName),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 pub enum Condition {
     Greater(Num),
     Less(Num),
@@ -59,15 +85,78 @@ pub enum Rule {
     Send(WorkflowName),
 }
 
+impl Rule {
+    pub fn process_part(&self, part: Part) -> Option<Next> {
+        match self {
+            Self::Accept => Some(Next::Accept),
+            Self::Reject => Some(Next::Reject),
+            Self::Send(name) => Some(Next::Send(name.clone())),
+            Self::Comparative {
+                condition,
+                next,
+                rating_label,
+            } => {
+                let value = part.label(*rating_label);
+
+                let applies = match *condition {
+                    Condition::Greater(than) => value > than,
+                    Condition::Less(than) => value < than,
+                };
+
+                if applies {
+                    Some(next.clone())
+                } else {
+                    // the rule does not apply, I don't know what to do,
+                    // move to the next
+                    None
+                }
+            }
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Ruleset(Vec<Rule>);
 
-#[derive(Debug)]
+impl std::ops::Deref for Ruleset {
+    type Target = Vec<Rule>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl Ruleset {
+    pub fn process_part(&self, part: Part) -> Next {
+        for rule in self.iter() {
+            match rule.process_part(part) {
+                None => continue,
+                Some(next) => return next,
+            }
+        }
+        unreachable!()
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
 pub struct Part {
     x: Num,
     m: Num,
     a: Num,
     s: Num,
+}
+
+impl Part {
+    pub fn label(self, label: RatingLabel) -> Num {
+        let Part { x, m, a, s } = self;
+        use RatingLabel::*;
+        match label {
+            X => x,
+            M => m,
+            A => a,
+            S => s,
+        }
+    }
 }
 
 pub type Parts = Vec<Part>;
